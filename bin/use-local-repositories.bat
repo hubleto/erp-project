ea327a3bb6f9cb703eba54a01cc0a4f189f8e150
Dusan <dusan.daniska@wai.sk>
2025-08-18 21:57:24 +0200

    added assets repository

--- bin/use-local-repositories.bat
+++ bin/use-local-repositories.bat
@@ -10,5 +10,9 @@ rem hubleto/framework
 call composer config repositories.hubleto/framework path %1\framework
 call composer require hubleto/framework dev-main
 
+rem hubleto/assets
+call composer config repositories.hubleto/assets path %1\assets
+call composer require hubleto/assets dev-main
+
 rem hubleto/react-ui
 call npm install file:%1\react-ui
