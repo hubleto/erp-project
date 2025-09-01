df3645071b1c91b09c88187715b3c8eec97ec87f
Michal Blava <michal.blava@gmail.com>
2025-09-01 09:42:56 +0200

    add linux scripts

---
+++ bin/use-local-repositories.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE_DIR="$1"
+
+composer config repositories.hubleto/erp path "$BASE_DIR/erp"
+composer require hubleto/erp:dev-main
+
+composer config repositories.hubleto/framework path "$BASE_DIR/framework"
+composer require hubleto/framework:dev-main
+
+composer config repositories.hubleto/assets path "$BASE_DIR/assets"
+composer require hubleto/assets:dev-main
+
+npm install "file:$BASE_DIR/react-ui"
