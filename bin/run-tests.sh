4c2539445f66326e8edd4a2ecf434d7829a37ec9
Dusan <dusan.daniska@wai.sk>
2025-09-22 10:00:26 +0200

    fixed tests

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
 "$SCRIPT_DIR/../vendor/bin/phpunit" --bootstrap test.php \
-    "$SCRIPT_DIR/../vendor/hubleto/main/tests" \
-    "$SCRIPT_DIR/../vendor/hubleto/apps" \
+    "$SCRIPT_DIR/../vendor/hubleto/erp/tests" \
+    "$SCRIPT_DIR/../vendor/hubleto/erp/apps" \
     "$SCRIPT_DIR/../tests"
--- test.php
+++ test.php
@@ -4,6 +4,5 @@
 require_once(__DIR__ . "/boot.php");
 
 // auth user with ID = 1
-$main->setParam('login', 'admin@example.com');
-$main->setParam('password', 'admin');
-$main->authProvider()->auth();
+$hubleto->router()->setRouteVars(['login' => 'admin@example.com', 'password' => 'admin']);
+$hubleto->authProvider()->auth();
