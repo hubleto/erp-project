5d2e88c4857ac9460eac7d94b1bf7caba0c3dec6
Dusan <dusan.daniska@wai.sk>
2026-01-10 17:43:39 +0100

    updated run-tests.bat

--- bin/run-tests.bat
+++ bin/run-tests.bat
@@ -1,1 +1,9 @@
-%~dp0..\vendor\bin\phpunit --bootstrap test.php %~dp0..\vendor\hubleto\erp\tests %~dp0..\vendor\hubleto\erp\apps %~dp0..\tests
+rem This script must be run from the project's root folder followingly:
+rem .\bin\run-tests.bat
+
+%~dp0..\vendor\bin\phpunit ^
+  --bootstrap test.php ^
+  --debug ^
+  %~dp0..\vendor\hubleto\erp\tests ^
+  %~dp0..\vendor\hubleto\erp\apps ^
+  %~dp0..\tests
