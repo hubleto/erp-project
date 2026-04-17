feed0eb7928732c57abafbdaac8dcd868e0bdc09
Michal Barnas <michal.barnas@univie.ac.at>
2026-04-17 14:16:47 +0200

    fixed releaseFolder and added appRepositories for loop

--- boot.php
+++ boot.php
@@ -1,7 +1,5 @@
 <?php
 
-set_time_limit(600);
-
 ini_set('html_errors', "1");
 ini_set('error_prepend_string', "<pre style='color: #333; font-face:monospace; font-size:8pt;'>");
 ini_set('error_append_string', "</pre>");
@@ -13,16 +11,16 @@ ini_set('error_reporting', E_ALL ^ E_NOTICE ^ E_WARNING);
 require_once(__DIR__ . "/ConfigEnv.php");
 
 // load autoloaders
-require($config['srcFolder'] . "/vendor/autoload.php");
+require(($config['releaseFolder'] ?? '.') . "/vendor/autoload.php");
 
 if (is_array($config['appRepositories']) && count($config['appRepositories']) > 0) {
-  foreach ($config['appRepositories'] as $appRepository) {
-    if (is_dir($appRepository)) {
-      require($appRepository . '/autoload.php');
+    foreach ($config['appRepositories'] as $appRepository) {
+        if (is_dir($appRepository)) {
+            require($appRepository . '/autoload.php');
+        }
     }
-  }
 }
 
-// boot
+// init main class
 $hubleto = new \Hubleto\Erp\Loader($config);
 $hubleto->init();
